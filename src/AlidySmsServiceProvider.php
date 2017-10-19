ca1012ec0b553135e5c415428e0455bbc5cc4051
David Sun <sunweihua@sunweihuadeiMac.local>
2017-10-19 13:05:56 +0800

    第一次递交

---
+++ src/AlidySmsServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaraMall\AlidySms;
+
+use Illuminate\Support\ServiceProvider;
+
+class AlidySmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {   
+
+        //发布配置文件
+        $this->publishes([
+            __DIR__.'/config/sms.php' => config_path('sms.php'),
+        ]);
+
+        //添加短信的验证
+        // Validator extensions
+        $this->app['validator']->extend('sms', function($attribute, $value, $parameters)
+        {
+            return (new Sms)->put('phone',request()->phone)->check($value);
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/sms.php', 'sms'
+        );
+    }
+}
---
+++ src/config/sms.php
@@ -0,0 +1,11 @@
+<?php
+return [
+	//id
+	'ACCESS_KEY_ID'=>'8nLLy6fFmoMGCfU3',
+	//秘钥
+	'ACCESS_KEY_SECRET'=>'jMVEAPo1v89CofjP0N8AOcexnMpLSW',
+	//短信签名
+	'signName'=>'php说验证码',
+	//短信模板编号
+	'templateCode'=>'SMS_91950092',
+];
