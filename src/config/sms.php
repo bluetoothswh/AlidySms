682225f067f93d0b2be164f9c893da8a92ff176d
David Sun <bluetooth_swh@163.com>
2017-10-19 13:36:20 +0800

    Update sms.php
    去掉demo里面阿里大于的参数信息

--- src/config/sms.php
+++ src/config/sms.php
@@ -1,11 +1,11 @@
 <?php
 return [
 	//id
-	'ACCESS_KEY_ID'=>'8nLLy6fFmoMGCfU3',
+	'ACCESS_KEY_ID'=>'',
 	//秘钥
-	'ACCESS_KEY_SECRET'=>'jMVEAPo1v89CofjP0N8AOcexnMpLSW',
+	'ACCESS_KEY_SECRET'=>'',
 	//短信签名
-	'signName'=>'php说验证码',
+	'signName'=>'',
 	//短信模板编号
-	'templateCode'=>'SMS_91950092',
+	'templateCode'=>'',
 ];
